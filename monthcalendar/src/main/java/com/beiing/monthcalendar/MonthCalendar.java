80b533adbc232d00ab47b0bfcd824a971b5618ff
Beiing <15764230067@163.com>
2017-05-23 15:42:18 +0800

    first commit

---
+++ monthcalendar/src/main/java/com/beiing/monthcalendar/MonthCalendar.java
@@ -0,0 +1,32 @@
+package com.beiing.monthcalendar;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+/**
+ * Created by linechen on 2017/5/23.<br/>
+ * 描述：
+ * </br>
+ */
+
+public class MonthCalendar extends LinearLayout{
+
+    public MonthCalendar(Context context) {
+        this(context, null);
+    }
+
+    public MonthCalendar(Context context, @Nullable AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public MonthCalendar(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+
+
+
+
+}
